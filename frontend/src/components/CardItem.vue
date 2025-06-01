3a48f771f836eded38d3a064f35a46710092c107
Adam Haikal <adamhaikalibrahim00@gmail.com>
2025-06-02 00:39:45 +0800

    Replace text with input when editing

--- frontend/src/components/CardItem.vue
+++ frontend/src/components/CardItem.vue
@@ -70,67 +70,67 @@ const handleDelete = async (id) => {
   <div
     class="bg-white space-x-1 rounded-lg px-2 border-2 border-gray-200 hover:bg-white/30 flex items-center">
     <!-- Checkbox for completed status -->
-    <button
-      v-if="isSubtask"
-      type="button"
-      name="completedButton"
-      id="completedButton"
-      :class="[
-        isCompleted
-          ? 'bg-green-500 border-green-600'
-          : 'bg-gray-100 border-gray-300',
-        'h-5 w-5 rounded-full border-2 appearance-none',
-      ]" />
+    <span :class="[!showForm ? 'inline-flex' : 'mb-14']">
+      <button
+        v-if="isSubtask"
+        type="button"
+        name="completedButton"
+        id="completedButton"
+        :class="[
+          isCompleted
+            ? 'bg-green-500 border-green-600'
+            : 'bg-gray-100 border-gray-300',
+          'h-5 w-5 rounded-full border-2 appearance-none',
+        ]" />
+    </span>
 
     <!-- Task content -->
     <section class="w-full ml-2">
-      <!-- Only display if it is task -->
-      <div v-if="!isSubtask">
-        <router-link :to="{ name: 'Subtasks', params: { id: tasksItem.id } }">
-          <div class="py-2">
-            <p
-              name="taskName"
-              class="text-md font-bold text-gray-300 dark:text-gray-900">
-              {{ tasksItem.name }}
-            </p>
-            <p class="text-xs text-gray-500 dark:text-gray-600 font-normal">
-              Last updated: {{ formattedDate }}
-            </p>
+      <div>
+        <!-- Display normal text if not in edit mode, otherwise display form -->
+        <template v-if="!showForm">
+          <router-link :to="{ name: 'Subtasks', params: { id: tasksItem.id } }">
+            <div class="py-2">
+              <p
+                name="taskName"
+                :class="[
+                  !isSubtask ? 'font-bold' : 'font-semibold',
+                  'text-md text-gray-300 dark:text-gray-900',
+                ]">
+                {{ tasksItem.name }}
+              </p>
+            </div>
+          </router-link>
+        </template>
+
+        <template v-else>
+          <div class="py-2 mr-4">
+            <form @submit.prevent="handleUpdate(tasksItem)" class="">
+              <Input
+                inputType="text"
+                v-model="tasksItem.name"
+                class="rounded-lg" />
+
+              <span class="flex mt-1 justify-end space-x-2">
+                <button
+                  type="submit"
+                  class="bg-teal-500 p-2 rounded-lg text-white text-xs cursor-pointer">
+                  Save
+                </button>
+                <button
+                  type="button"
+                  @click="handleCancel(tasksItem.id)"
+                  class="text-gray-500 cursor-pointer hover:text-red-400">
+                  Cancel
+                </button>
+              </span>
+            </form>
           </div>
-        </router-link>
-      </div>
+        </template>
 
-      <!-- Only display subtask info if it exists -->
-      <div v-if="isSubtask" class="py-4">
-        <p class="text-md font-semibold text-gray-300 dark:text-gray-900">
-          {{ tasksItem.name }}
+        <p class="text-xs text-gray-500 dark:text-gray-600 font-normal mb-1">
+          Last updated: {{ formattedDate }}
         </p>
-      </div>
-
-      <!-- Update form -->
-      <div v-if="showForm" class="mr-2 mb-2">
-        <form @submit.prevent="handleUpdate(tasksItem)" class="flex space-x-2">
-          <Input
-            inputType="text"
-            v-model="tasksItem.name"
-            class="rounded-lg"
-            required />
-
-          <!-- Submit form button -->
-          <button
-            type="submit"
-            class="bg-teal-500 p-1 rounded-lg text-white text-xs cursor-pointer">
-            Add Task
-          </button>
-
-          <!-- Close form button -->
-          <button
-            type="button"
-            @click="handleCancel(tasksItem.id)"
-            class="text-gray-500 cursor-pointer hover:text-red-400">
-            Cancel
-          </button>
-        </form>
       </div>
     </section>
 
