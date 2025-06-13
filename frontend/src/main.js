89e58a36a201d1ae6cf4e7854453f83c8264b0f4
Adam Haikal <adamhaikalibrahim00@gmail.com>
2025-06-13 23:58:13 +0800

    Added toast for create, update & delete

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,9 @@ import { createPinia } from "pinia";
 import "./style.css";
 import App from "@/App.vue";
 import router from "@/router";
+import Vue3Toastify from "vue3-toastify";
+import 'vue3-toastify/dist/index.css';
 
 const pinia = createPinia();
 
-createApp(App).use(router).use(pinia).mount("#app");
+createApp(App).use(router).use(pinia).use(Vue3Toastify).mount("#app");
